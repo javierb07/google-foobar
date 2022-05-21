9c693bd346509fab9c1405da1fc6e6d455507a06
Javier Belmonte <43077436+javierb07@users.noreply.github.com>
2022-05-21 18:18:52 -0300

    Add comments to challenge 4

--- 4-fuel-injection-perfection/solution.py
+++ 4-fuel-injection-perfection/solution.py
@@ -1,15 +1,23 @@
 def solution(n):
+    # Convert the input to integer and return 0 if it's 1 or less
     n = int(n)
     if n <= 1:
         return 0
     operations = 0
+    # Keep adding operations until the number of pellets is 1
     while n != 1:
+        # If the number of pellets is even divide by 2 and add one operation 
         if (n % 2 == 0):
             n /= 2
             operations += 1
         else:
+            # If number is 3 remove one pellet (special case)
             if n == 3:
                 n -= 1
+            # If number is odd and different to 3, convert the 
+            # number + 1 and - 1 to binary. The number of zeroes in 
+            # the binary representation will determine how many times
+            # the number will be odd after it's divided by 2 (preferred operation) 
             else:
                 binaryUp = bin(n + 1)
                 binaryDown = bin(n - 1)
@@ -17,6 +25,7 @@ def solution(n):
                 downTrailingZeroes = 0
                 indexUp = len(binaryUp) - 1
                 indexDown = len(binaryDown) - 1
+                # Determine if the number + 1 or number - 1 has more trailing zeroes
                 while upTrailingZeroes == downTrailingZeroes:
                     if binaryUp[indexUp] == "0":
                         upTrailingZeroes += 1
@@ -24,6 +33,8 @@ def solution(n):
                         downTrailingZeroes += 1
                     indexUp -= 1
                     indexDown -= 1
+                # Whichever number has more trailing zeroes will require less overall
+                # operation in the algorithm
                 if upTrailingZeroes > downTrailingZeroes:
                     n += 1
                 else:
